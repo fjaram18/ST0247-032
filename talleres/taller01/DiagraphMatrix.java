4bdf907af47fe20755ec9415f975082bf7b373e6
ditrefftzr <30441097+ditrefftzr@users.noreply.github.com>
2018-07-20 13:25:42 -0300

    Update DiagraphMatrix.java

--- talleres/taller01/DiagraphMatrix.java
+++ talleres/taller01/DiagraphMatrix.java
@@ -1,23 +1,25 @@
 import java.util.ArrayList;
 /**
- *  NOTA: Se decidió eliminar el atributo "Weight" ya que
-    no aporta para la creación y uso de la matriz. Si se necesitara
-    hayar un flujo máximo, por ejemplo, se tendría en cuenta.
+ * 
  * @author ditrefftzr, fjaram18
  */
 
 public class DiagraphMatrix {
     private int[][] matrix ; 
-
+        /** Class constructor 
+         */
 	public DiagraphMatrix(int size) {
             matrix = new int [size][size] ;
 		
 	}
-        
-	public void addArc(int source, int destination) {
-                matrix[source][destination]= 1 ;
+        /** creates an arc between a source and a destination. the arc is given
+         * a certain value
+         */
+	public void addArc(int source, int destination, int weight) {
+                matrix[source][destination]= weight ;
 	}
-
+        /** Selects a vertix and returns every successor it has. 
+         */
 	public ArrayList<Integer> getSuccessors(int vertex) {
             ArrayList <Integer> lista = new ArrayList<>() ;      
                  for (int i=0 ; i< matrix[vertex].length ; i++) {
@@ -27,4 +29,8 @@ public class DiagraphMatrix {
                 }
                  return lista ;
 	}
+        
+        public Integer getWeight(int source, int destination){
+            return matrix [source][destination] ;
+        }
 }
