d32ce661bf59b4cf4db79ccc893b19c4b85a1b96
fjaram18 <41440657+fjaram18@users.noreply.github.com>
2018-07-26 11:55:13 -0500

    Update Taller2.java
    Reinas sin terminar

--- talleres/taller02/Taller2.java
+++ talleres/taller02/Taller2.java
@@ -59,12 +59,55 @@ public class Taller2 {
     }
 
     public static boolean esValido(int[] tablero) {
-        return false;
-        // complete...
+        int n = tablero.length;
+        
+        //recorre cada columna (deberia haber una reina por col) del arreglo
+        for (int i = 0; i < n; ++i) {
+            //fila de la reina en la columna "i"
+            int pos = tablero[i];
+            //mira las diagonales a la derecha de una reina
+            for (int j = 1; j < n - i; j++) {
+                int up = pos - j;
+                int down = pos + j;
+                if( tablero[i + j] == up || tablero[i + j] == down) {
+                    return false;
+                }
+            }
+            
+            //mira las diagonales a la izquierda de una reina
+            for (int j = 1; j < i - n; j++) {
+                int up = pos - j;
+                int down = pos + j;
+                if( tablero[i - j] == up || tablero[i - j] == down) {
+                    return false;
+                }
+            }
+        }
+        //mira las horizontales del tablero
+        for (int i = 0; i < n; i++) {
+            for (int j = 1 + i; j < n; j++) {
+                if (tablero[i] == tablero[j]) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
     }
 
     public static int queens(int n) {
-        return 0;
+        int cont = 0;
+        ArrayList<int[]> permutaciones = new ArrayList<int[]>();
+        int[] chess; 
+        
+        //Crear todas las posibles posiciones de n-reinas (1 por col)
+        for (int i = 0; i < n; i++) {
+            for(int j = 0; j < n; i++) {
+                
+            }
+        }
+        
+        return cont;
         // complete...
     }
     
