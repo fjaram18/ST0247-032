6bbfcd59e5548d23128257f0d0becb43dbc2e3f1
fjaram18 <41440657+fjaram18@users.noreply.github.com>
2018-08-24 18:10:16 -0500

    Update taller6.java

--- talleres/taller06/taller6.java
+++ talleres/taller06/taller6.java
@@ -10,7 +10,7 @@ public class Taller6 {
     public static int[] cambioGreedy(int n, int[] denominaciones) {
         int[] respuesta = new int[denominaciones.length];
         boolean esNull = false;
-        
+
         //Algoritmo voraz para encontrar la vuelta minima en monedas 
         for (int i = 0; i < denominaciones.length; i ++) {
             int temp = 0;
@@ -18,43 +18,48 @@ public class Taller6 {
             n -= temp * denominaciones[i];
             respuesta[i] = temp;
         }
-        
+
         //Cambio no-exacto
         if (n != 0) {
             respuesta = null;
         }
-        
+
         return respuesta;
     }
 
-    
     public static int recorrido(Digraph g) {
-        
         int[] visitados = new int[g.size()];
         int verticeIni = 0;
         visitados[0] = 1;
         int respuesta = 0;
-        
+
         for (int i = 0; i < g.size(); i++) {
-            
-            ArrayList<Integer> sucesores = g.getSuccessors(verticeIni);
-            int max = 0;
-            int verticeFin = 0;
-            
-            for (int j = 0; j < sucesores.size(); j++) {
-                int peso = g.getWeight(verticeIni, j);
-                if (max < peso && visitados[j] == 0) {
-                    max = peso;
-                    verticeFin = j;
-                }
+            if ( i == g.size() - 1) {
+                respuesta += g.getWeight(verticeIni, 0);
+                break;
             }
             
+            
+            ArrayList<Integer> sucesores = g.getSuccessors(verticeIni);
+            int min = 2147483647;
+            int verticeFin = 0;
+
+            for (int j = 0; j < sucesores.size(); j++) {
+                int peso = g.getWeight(verticeIni, sucesores.get(j));
+                if (min > peso && visitados[sucesores.get(j)] == 0) {
+                    min = peso;
+                    verticeFin = sucesores.get(j);
+                }
+            }
+
             visitados[verticeFin] = 1;
             verticeIni = verticeFin;
-            respuesta += max;
+            respuesta += min;
+            
+            
         }
-        
+
         return respuesta;
     }
-    
+
 }
